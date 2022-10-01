cc677c7e53f1e3078e12bbcbc4ec5d726a5247cf
roooff <114150832+roooff@users.noreply.github.com>
2022-10-01 18:05:26 +0300

    Adding new shit

---
+++ Arrays Exercise/ArraysEx/2. Common Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Arrays Exercise/ArraysEx/3. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
