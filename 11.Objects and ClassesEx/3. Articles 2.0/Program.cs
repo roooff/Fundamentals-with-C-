2aaade9f4fde59d3a1e3fe7be2ced208772ce683
roooff <114150832+roooff@users.noreply.github.com>
2022-10-29 15:41:05 +0300

    Iskam da se samoubiq prepish wsihcko
    wdwdw

--- 11.Objects and ClassesEx/3. Articles 2.0/Program.cs
+++ 11.Objects and ClassesEx/3. Articles 2.0/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
-
-namespace _3._Articles_2._0
+﻿namespace _3._Articles_2._0
 {
+    using System;
+    using System.Collections.Generic;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfArticles = int.Parse(Console.ReadLine());
+
+            List<Article> articles = new List<Article>();
+
+            for (int i = 0; i < numberOfArticles; i++)
+            {
+                string[] info = Console.ReadLine().Split(", ");
+                Article article = new Article(
+                    info[0], info[1], info[2]);
+                articles.Add(article);
+            }
+            string command = Console.ReadLine();
+
+            foreach (Article article in articles)
+            {
+                article.ToString(article.Title, article.Content, article.Author);
+            }
+        }
+
+        class Article
+        {
+            public Article(string title, string content, string author)
+            {
+                this.Title = title;
+                this.Content = content;
+                this.Author = author;
+            }
+            public string Title { get; set; }
+
+            public string Content { get; set; }
+
+            public string Author { get; set; }
+
+
+            public void ToString(string title, string content, string author)
+            {
+                Console.WriteLine($"{title} - {content}: {author}");
+            }
         }
     }
 }
