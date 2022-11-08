6cba984847d07ccd124068aab409de09aeddd213
roooff <114150832+roooff@users.noreply.github.com>
2022-11-08 16:43:18 +0200

    Exercise 1 and Recap

---
+++ 1. Count Real Numbers/Recap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
