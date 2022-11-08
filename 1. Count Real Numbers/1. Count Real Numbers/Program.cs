45ca616c0439af1fe3d5e4380c137ad1e7eedacc
roooff <114150832+roooff@users.noreply.github.com>
2022-11-08 15:11:10 +0200

    The first

--- 1. Count Real Numbers/1. Count Real Numbers/Program.cs
+++ 1. Count Real Numbers/1. Count Real Numbers/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
-namespace _1._Count_Real_Numbers
+﻿namespace _1._Count_Real_Numbers
 {
+    using System;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
