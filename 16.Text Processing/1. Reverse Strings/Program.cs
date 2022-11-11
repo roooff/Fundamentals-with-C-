29ad340b5cd5200b67c8d13fb9ada4cc4ff98f1d
roooff <114150832+roooff@users.noreply.github.com>
2022-11-11 15:22:30 +0200

    Ponqkoga ne mi se wika

---
+++ 16.Text Processing/1. Reverse Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _1._Reverse_Strings
+{
+    using System;
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ 16.Text Processing/2. Repeat Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _2._Repeat_Strings
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] words = Console.ReadLine().Split();
+            string result = string.Empty;
+            foreach (var currword in words)
+            {
+                for (int i = 0; i < currword.Length; i++)
+                {
+                    result += currword;
+                }
+            
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
