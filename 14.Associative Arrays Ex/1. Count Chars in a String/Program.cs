3e252bbff77f158f35a0c5247334195910bbe018
roooff <114150832+roooff@users.noreply.github.com>
2022-11-11 13:58:34 +0200

    SMURTTA IZGLEJDA PO LESNA OT TAQ SHIBANA ZADACHA

--- 14.Associative Arrays Ex/1. Count Chars in a String/Program.cs
+++ 14.Associative Arrays Ex/1. Count Chars in a String/Program.cs
@@ -1,11 +1,35 @@
 ﻿namespace _1._Count_Chars_in_a_String
 {
     using System;
+    using System.Collections.Generic;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+                Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+                string text = Console.ReadLine();
+                for (int n = 0; n < text.Length; n++)
+                {
+                    if (text[n] == ' ') continue;
+                    if (!charCount.ContainsKey(text[n]))
+                    {
+                        charCount.Add(text[n], 0);
+                    }
+
+                    charCount[text[n]]++;
+                }
+
+                foreach (var item in charCount)
+                {
+                    Console.WriteLine($"{item.Key} -> {item.Value}");
+                }
+            }
         }
     }
-}
+
+
+    
+
