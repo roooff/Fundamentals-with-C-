ebf6a3d31e42d3184e66e472985b4d63b24f253f
roooff <114150832+roooff@users.noreply.github.com>
2022-11-11 12:11:07 +0200

    AMI AKO SE SAMOUBIQ

--- 14.Associative Arrays Ex/2. A Miner Task/Program.cs
+++ 14.Associative Arrays Ex/2. A Miner Task/Program.cs
@@ -1,11 +1,28 @@
 ﻿namespace _2._A_Miner_Task
 {
     using System;
+    using System.Collections.Generic;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            string resource;
+            while ((resource = Console.ReadLine()) !="stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(resource))
+                {
+                    resources[resource] = 0;
+                }
+                resources[resource]+= quantity;
+            }
+            foreach (var rqp in resources)
+            {
+                Console.WriteLine($"{rqp.Key} -> {rqp.Value}");
+            }
         }
     }
 }
