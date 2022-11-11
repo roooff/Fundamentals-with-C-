d4c11dead4576d53ffad5a7f67db3d91b783ad59
roooff <114150832+roooff@users.noreply.github.com>
2022-11-11 14:02:26 +0200

    DANO NAKOV DA SE ZADAWI S KRASTAVICI

--- 14.Associative Arrays Ex/6. Student Academy/Program.cs
+++ 14.Associative Arrays Ex/6. Student Academy/Program.cs
@@ -1,11 +1,48 @@
 ﻿namespace _6._Student_Academy
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            
+                Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+                int n = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < n; i++)
+                {
+                    string name = Console.ReadLine();
+                    double grade = double.Parse(Console.ReadLine());
+
+                    if (!students.ContainsKey(name))
+                    {
+                        students.Add(name, new List<double>());
+                    }
+
+                    students[name].Add(grade);
+                }
+
+                Dictionary<string, double> bestStudents = new Dictionary<string, double>();
+                foreach (var student in students)
+                {
+                    double current = student.Value.Average();
+                    if (current >= 4.50)
+                    {
+                        bestStudents.Add(student.Key, current);
+                    }
+                }
+
+                foreach (var student in bestStudents)
+                {
+                    Console.WriteLine($"{student.Key} -> {student.Value:f2}");
+                }
+            }
         }
     }
-}
+
+    
+
