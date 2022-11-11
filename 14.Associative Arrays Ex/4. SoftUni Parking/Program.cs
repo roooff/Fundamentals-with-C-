ea30aad19f5bc7bdf1d5b1734f05033a9670c31c
roooff <114150832+roooff@users.noreply.github.com>
2022-11-11 13:51:01 +0200

    OLEEE ISKAM DA SE SAAMOUBIQ

--- 14.Associative Arrays Ex/4. SoftUni Parking/Program.cs
+++ 14.Associative Arrays Ex/4. SoftUni Parking/Program.cs
@@ -1,11 +1,52 @@
 ﻿namespace _4._SoftUni_Parking
 {
     using System;
+    using System.Collections.Generic;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string,string> register = new Dictionary<string,string>();  
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string cmdType = cmdArgs[0];
+                string username = cmdArgs[1];
+                if (cmdType=="register")
+                {
+                    string licenseNumber = cmdArgs[2];
+                    if (!register.ContainsKey(username))
+                    {
+                        register[username] = licenseNumber;
+                        Console.WriteLine($"{username} registered {licenseNumber} successfully");
+                    }
+                    else
+                    {
+                        string registeredNumber = register[username];
+                        Console.WriteLine($"ERROR: already registered with plate number {registeredNumber}");
+                    }
+
+                }
+                else if (cmdType=="unregister")
+                {
+                    if (!register.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        register.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+            foreach (var dictionary in register)
+            {
+                Console.WriteLine($"{dictionary.Key} => {dictionary.Value}");
+            }
         }
     }
 }
--- 14.Associative Arrays Ex/5. Courses/Program.cs
+++ 14.Associative Arrays Ex/5. Courses/Program.cs
@@ -1,11 +1,41 @@
 ﻿namespace _5._Courses
 {
     using System;
+    using System.Collections.Generic;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Course name 
+            // Key-> Value
+            Dictionary<string, List<string>> coursesInfo = new Dictionary<string, List<string>>();
+            string commnad;
+            while ((commnad= Console.ReadLine()) !="end")
+            {
+                string[] cmdArgs = commnad.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+                string courseName = cmdArgs[0];
+                string studentName = cmdArgs[1];
+
+                if (!coursesInfo.ContainsKey(courseName))
+                {
+                    coursesInfo[courseName] = new List<string>();
+                }
+                coursesInfo[courseName].Add(studentName);
+            }
+            foreach ( var kvp in coursesInfo)
+            {
+                string courseName = kvp.Key;
+                List<string> students = kvp.Value;
+
+                Console.WriteLine($"{courseName}: {students.Count}");
+                foreach (string studentName  in students)
+                {
+                    Console.WriteLine($"-- {studentName}");
+                }
+
+            }
+
         }
     }
 }
