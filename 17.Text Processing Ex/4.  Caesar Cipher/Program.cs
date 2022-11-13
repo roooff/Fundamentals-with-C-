7d60bfa134eec08b099857d10c3cd1b2d3742bc5
roooff <114150832+roooff@users.noreply.github.com>
2022-11-13 14:29:48 +0200

    AAA

--- 17.Text Processing Ex/4.  Caesar Cipher/Program.cs
+++ 17.Text Processing Ex/4.  Caesar Cipher/Program.cs
@@ -5,7 +5,16 @@
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+            string encryptedString = string.Empty;
+            foreach (char currChar in input)
+            {
+                int currPosition = currChar;
+                currPosition += 3;
+                encryptedString += (char)currPosition;
+            }
+            Console.WriteLine(encryptedString);
+
         }
     }
 }
