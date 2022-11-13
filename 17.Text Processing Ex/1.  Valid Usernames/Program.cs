50d6c8fd63288bcebb25ebbdd70914292783a132
roooff <114150832+roooff@users.noreply.github.com>
2022-11-13 12:52:59 +0200

    Ponqkoga jivota e putka

---
+++ 17.Text Processing Ex/1.  Valid Usernames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.__Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 17.Text Processing Ex/2.Character Multiplier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2.Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
