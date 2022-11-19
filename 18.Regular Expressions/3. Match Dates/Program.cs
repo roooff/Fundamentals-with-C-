b07023404f5ca52fd26fe5ff87866d9c86e9f5ea
roooff <114150832+roooff@users.noreply.github.com>
2022-11-19 16:18:55 +0200

    Update Program.cs

--- 18.Regular Expressions/3. Match Dates/Program.cs
+++ 18.Regular Expressions/3. Match Dates/Program.cs
@@ -1,11 +1,20 @@
 ﻿namespace _3._Match_Dates
 {
     using System;
+    using System.Text.RegularExpressions;
 
     internal class Program
     {
         private static void Main(string[] args)
         {
+            string pattern = @"(?<day>\d{2})(\.|-|/)(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})";
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollection = regex.Matches(input);
+            foreach (Match item in matchCollection)
+            {
+                Console.WriteLine(item.Value);
+            }
         }
     }
 }
