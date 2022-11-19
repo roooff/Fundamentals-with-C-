686e8ac99b314c96082e0faa21d9ad831d0ca349
roooff <114150832+roooff@users.noreply.github.com>
2022-11-19 16:35:19 +0200

    Bibi bom bi bi boom

---
+++ 19.Regular Expressions Ex/2. Race/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 19.Regular Expressions Ex/3. SoftUni Bar Income/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3._SoftUni_Bar_Income
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
