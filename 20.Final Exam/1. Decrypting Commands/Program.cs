119d979f28b5a7c35c876c46cae8e00417172aa7
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2022-12-04 11:51:37 +0200

    sdFSDFFSSFS

---
+++ 20.Final Exam/1. Decrypting Commands/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Decrypting_Commands
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // decrypting command
+
+            string strmod = Console.ReadLine();
+
+            while (true)
+            {
+                string[] cmd = Console.ReadLine().Split(new[] { ' ' }).ToArray();
+
+                if (cmd[0] == "Finish")
+                    break;
+
+                if (cmd[0] == "Replace")
+                {
+                    string currentChar = cmd[1];
+                    string newChar = cmd[2];
+                    strmod = strmod.Replace(currentChar, newChar);
+                    Console.WriteLine(strmod);
+                }
+
+                if (cmd[0] == "Cut")
+                {
+                    int startIndex = int.Parse(cmd[1]);
+                    int endIndex = int.Parse(cmd[2]);
+                    int length = Math.Abs(startIndex - endIndex) + 1;
+
+                    if (startIndex < 0 || endIndex > strmod.Length)
+                    {
+                        Console.WriteLine("Invalid indices!");
+                        continue;
+                    }
+
+                    strmod = strmod.Remove(startIndex, length);
+                    Console.WriteLine(strmod);
+                }
+
+                if (cmd[0] == "Make")
+                {
+                    if (cmd[1] == "Upper")
+                    {
+                        strmod = strmod.ToUpper();
+                    }
+
+                    if (cmd[1] == "Lower")
+                    {
+                        strmod = strmod.ToLower();
+                    }
+
+                    Console.WriteLine(strmod);
+                }
+
+                if (cmd[0] == "Check")
+                {
+                    Console.WriteLine((strmod.Contains(cmd[1]) ? "Message contains " + cmd[1] : "Message doesn't contain " + cmd[1]));
+                }
+
+                if (cmd[0] == "Sum")
+                {
+                    int startIndex = int.Parse(cmd[1]);
+                    int endIndex = int.Parse(cmd[2]);
+
+                    if (startIndex < 0 || endIndex > strmod.Length)
+                    {
+                        Console.WriteLine("Invalid indices!");
+                        continue;
+                    }
+
+                    int asciiSum = 0;
+
+                    for (int i = startIndex; i <= endIndex; i++)
+                    {
+                        asciiSum += strmod[i];
+                    }
+
+                    Console.WriteLine(asciiSum);
+                }
+            }
+        }
+    }
+}
---
+++ 20.Final Exam/2.Boss Rush/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2.Boss_Rush
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var regex = new Regex(@"\|(?<Name>[A-Z]{4,})\|\:\#(?<Title>[A-Za-z]+\s[A-Za-z]+)\#");
+                var mach = regex.Match(input);
+
+                if (mach.Success)
+                {
+                    var name = mach.Groups["Name"].Value;
+                    var title = mach.Groups["Title"].Value;
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">> Strength: {name.Length}");
+                    Console.WriteLine($">> Armor: {title.Length}");
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
+        }
+    }
+}
