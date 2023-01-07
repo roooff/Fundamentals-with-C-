7036de291b25e6b6024659cb1384bdc9d45c87ce
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2023-01-07 15:48:01 +0200

    a

--- 20.Final Exam/3.Wild Zoo/Program.cs
+++ 20.Final Exam/3.Wild Zoo/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _3.Wild_Zoo
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool istrue = true;
+
+            while (istrue)
+            {
+                string input = Console.ReadLine();
+                if (input == "EndDay")
+                {
+                    istrue = false;
+                    break;
+                }
+                string[] cmd = Console.ReadLine().Split("-", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if (cmd[0] == "Add:")
+                {
+                    string animalName = cmd[1];
+                    int food = int.Parse(cmd[2]);
+                    string area = cmd[3];
+                    var animal = new Dictionary<string, int>();
+                    if (animal.ContainsKey(animalName))
+                    {
+                        int oldfood = animal[animalName];
+                        animal[animalName] = oldfood + food;
+                    }
+                }
+                else if (cmd[0] == "Feed:")
+                {
+                    string animalName = cmd[1];
+                    string givenFodd = cmd[2];
+                }
+            }
         }
     }
 }
